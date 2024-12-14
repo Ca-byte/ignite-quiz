b67293e9268def96f103437cddc1924b9c71b5d6
Caroline Vieira <carolinesanvieira@gmail.com>
2024-12-14 20:45:47 +0100

    animation flow

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -144,6 +144,7 @@ export function Quiz() {
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
       setStatusReply(1)
       setPoints(prevState => prevState + 1);
+      handleNextQuestion();
     } else {
       setStatusReply(2)
       shakeAnimation();
@@ -170,7 +171,12 @@ export function Quiz() {
   function shakeAnimation() {
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }), 
-      withTiming(0)
+      withTiming(0, undefined, (Finished)=> {
+        'worklet';
+        if(Finished){
+          runOnJS(handleNextQuestion)();
+        }
+      })
     )
   }
 
@@ -234,6 +240,7 @@ export function Quiz() {
               question={quiz.questions[currentQuestion]}
               alternativeSelected={alternativeSelected}
               setAlternativeSelected={setAlternativeSelected}
+              onUnmount={() => setStatusReply(0)}
             />
           </Animated.View>
         </GestureDetector>
