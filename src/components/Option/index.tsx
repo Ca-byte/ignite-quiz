6fd00e1856baf3e811fc6cf8edd52301d3553b5c
Caroline Vieira <carolinesanvieira@gmail.com>
2024-12-16 22:55:00 +0100

    option done

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,6 +1,8 @@
 import {
   BlurMask,
   Canvas,
+  Circle,
+  Easing,
   Path,
   runTiming,
   Skia,
@@ -21,17 +23,20 @@ const CHECK_STROKE = 2;
 
 export function Option({ checked, title, ...rest }: Props) {
   const percentage = useValue(0);
-
+  const circle = useValue(0);
   const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
+  const CENTER_CIRCLE = RADIUS / 2;
   const path = Skia.Path.Make();
   path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS);
 
   useEffect(()=> {
     if(checked){
       runTiming(percentage, 1, { duration: 700 });
+      runTiming(circle, CENTER_CIRCLE, { easing: Easing.bounce });
 
     } else{
       runTiming(percentage, 0, { duration: 700 });
+      runTiming(circle, 0, { duration: 300 });
     }
 
   }, [checked])
@@ -62,8 +67,17 @@ export function Option({ checked, title, ...rest }: Props) {
           strokeWidth={CHECK_STROKE}
           start={0}
           end={percentage}
-        />
+          >
         <BlurMask blur={1} style="solid" />
+        </Path>
+        <Circle
+          cx={CHECK_SIZE}
+          cy={CHECK_SIZE}
+          r={circle}
+          color={THEME.COLORS.BRAND_LIGHT}
+          >
+          <BlurMask blur={4} style="solid" />
+        </Circle>
       </Canvas>
     </TouchableOpacity>
   );
